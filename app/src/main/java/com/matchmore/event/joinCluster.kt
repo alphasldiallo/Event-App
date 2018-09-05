23b7ae57f86685c13f04ab406e4829d640133cfe
Alpha Diallo <alphadiallo009@gmail.com>
2018-09-05 12:23:05 +0200

    Find your friend functionnality almost done

---
+++ app/src/main/java/com/matchmore/event/joinCluster.kt
@@ -0,0 +1,12 @@
+package com.matchmore.event
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class joinCluster : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.join_cluster)
+    }
+}
